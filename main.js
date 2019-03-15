b400dd603c524850731f7231ba3d5ff4068c094a
DrewVargas <41808076+DrewVargas@users.noreply.github.com>
2019-03-15 01:22:31 -0400

    Finished changes to table CSS

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const timer = document.getElementById('timer');
 const toggleBtn = document.getElementById('toggle');
 const resetBtn = document.getElementById('reset');
+const stopwatchContainer = document.querySelector('.stopwatch-container');
 
 const watch = new Stopwatch(timer);
 
@@ -9,10 +10,12 @@ function toggleTimer() {
     watch.start();
     toggleBtn.textContent = 'Stop';
     toggleBtn.classList.add('stop');
+    stopwatchContainer.classList.add('start');
   } else {
     watch.stop();
     toggleBtn.textContent = 'Start';
     toggleBtn.classList.remove('stop');
+    stopwatchContainer.classList.remove('start');
   }
 }
 
